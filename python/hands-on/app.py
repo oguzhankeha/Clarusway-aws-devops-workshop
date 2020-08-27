2450e0770416e8978e8e80d41dc40f06826ea7fe
oguzhankeha <oguzhankeha@gmail.com>
2020-08-27 12:12:49 +0200

    ders2

--- python/hands-on/app.py
+++ python/hands-on/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def head():
-    return render_template("index.html", number1 = 12, number2 = 34)
+def hello():
+    return "Hello World!"
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=80)
